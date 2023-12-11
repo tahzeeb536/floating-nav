cb0e03b273f56fbf6e10caea4c8cbf42b05741ff
Tahzeeb Sattar <tahzeeb@bitcards.com>
2023-12-11 16:39:19 +0500

    loader added

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
-
 const burger = gsap.timeline();
+const loader = gsap.timeline();
+
+document.addEventListener("DOMContentLoaded", function () {
+    loader.to("#site-loader", { 
+      duration: .75, 
+      x: "100%", 
+      ease: "power2.out" 
+    })
+    .fromTo('#burger', 
+    {
+      duration: 0.3,
+      right: '-60px',
+    },
+    { 
+      duration: .75,
+      right: '0px',
+    });
+});
 
 ScrollTrigger.create({
   start: 0,
